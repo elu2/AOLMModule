dc6a18e319566e74dca0693ec0d72828f9e06bfc
elu2 <60558250+elu2@users.noreply.github.com>
2022-04-19 16:39:10 -0700

    Update config.R

--- config.R
+++ config.R
@@ -5,6 +5,9 @@ grid_path <- "./grids/"
 if (!(file.exists(grid_path))){dir.create(grid_path)}
 # Path of csv to write validation predictions to
 val_csv <- "./validationOut.csv"
+
+# For frequency validation
+n_iter <- 100
 freq_val_csv <- "./freqValOut.csv"
 
 # glmnet "family" argument
